427bd2876c10a353cf31d3940831ec5e7930b017
General0917 <ht13a037@oecu.jp>
2023-05-28 06:14:58 +0900

    chapter4の練習問題の解答

---
+++ chap04/Practice/p_if.php
@@ -0,0 +1,29 @@
+<?php
+// if文を使用した場合の条件分岐
+// $score_if = 75;
+// if ($score_if >= 90) {
+//     print '優';
+// } elseif ($score_if >= 70) {
+//     print '良';
+// } elseif ($score_if >= 50) {
+//     print '可';
+// } else {
+//     print '不可';
+// }
+
+// switch文を使用した場合の条件分岐
+$score_switch = 75;
+switch ($score_switch) {
+    case $score_switch >= 90:
+        print '優';
+        break;
+    case $score_switch >= 70;
+        print '良';
+        break;
+    case $score_switch >= 50;
+        print '可';
+        break;
+    default:
+        print '不可';
+        break;
+}
