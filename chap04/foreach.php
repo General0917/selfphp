cb6686f43fba2bb713e7ed236f4a9aee6eb96de2
General0917 <ht13a037@oecu.jp>
2023-06-07 21:19:50 +0900

    chapter4の続き

---
+++ chap04/foreach.php
@@ -0,0 +1,6 @@
+<?php
+$data = ['102 Duel', '103 Buster', '105 Strike', '207 Blitz', '303 Aegis'];
+
+foreach ($data as $GAT_X) {
+    print "GAT-X{$GAT_X}<br />";
+}
---
+++ chap04/foreach_assoc.php
@@ -0,0 +1,12 @@
+<?php
+$data = [
+    '102' => 'Duel',
+    '103' => 'Buster',
+    '105' => 'Strike',
+    '207' => 'Blitz',
+    '303' => 'Aegis'
+];
+
+foreach ($data as $key => $values) {
+    print "GAT-X{$key} {$values}<br />";
+}
